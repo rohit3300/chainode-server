9580626bf982e3bb7d3a9ed931130b4e4a02e2aa
rohit3300 <rohitkahar33@gmail.com>
2026-06-07 18:57:38 +0530

    chore: update status message in root endpoint response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.json({message: 'Server is healthy v4.2', status: 'Github Actions'});
+    res.json({message: 'Server is healthy v4.2', status: 'Github Actions hurrayy'});
 });
 
 app.listen(PORT, () => {
