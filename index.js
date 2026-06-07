899c28a242c20ba65401f66228484c16fa682ed6
rohit3300 <rohitkahar33@gmail.com>
2026-06-07 18:42:21 +0530

    feat: implement GitHub Actions CI/CD pipeline and update server response status

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.json({message: 'Server is healthy v3.1'});
+    res.json({message: 'Server is healthy v4.1', status: 'Github Actions'});
 });
 
 app.listen(PORT, () => {
